c85182258cb91d5d1be770327cf1780d9d99f922
Jaejoon Jung <jaejoonjung@inslab.co.kr>
2023-11-24 17:29:33 +0900

    a4b complete

--- src/bin/a4a.rs
+++ src/bin/a4a.rs
@@ -7,4 +7,11 @@
 // * Use a variable set to either true or false
 // * Use a match expression to determine which message to display
 
-fn main() {}
+fn main() {
+    let var = true;
+    match var {
+        true => println!("its true"),
+        false => println!("its false"),
+        
+    }
+}
