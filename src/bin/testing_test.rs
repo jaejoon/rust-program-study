2d1f14f94df6f06d07be0780c55d86df75ecb3ec
Jaejoon Jung <jaejoonjung@inslab.co.kr>
2024-01-04 00:40:49 +0900

    test completed

---
+++ src/bin/testing_test.rs
@@ -0,0 +1,19 @@
+fn all_caps(word: &str) -> String {
+    word.to_uppercase()
+}
+
+fn main() {
+    
+}
+
+#[cfg(test)]
+mod test {
+    use crate::all_caps;
+
+    #[test]
+    fn check_all_caps() {
+        let result = all_caps("hello");
+        let expected = String::from("hello");
+        assert_eq!(result, expected,"string should be all uppercase");
+    }
+}
